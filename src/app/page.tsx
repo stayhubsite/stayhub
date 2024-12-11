fb90bfa16fb340ba6e71a8a35977eaa7fd41420f
stayhubsite <stayhubsite@gmail.com>
2024-12-11 13:26:28 -0500

    integrated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,14 @@
 import { Box } from "@mui/material";
 
 import Navbar from "@/components/Navbar";
+import ContactForm from "@/components/ContactForm";
 
 
 export default function HomePage() {
   return (
     <Box>
       <Navbar />
-  
+      <ContactForm />
     </Box>
   );
 }
